0accd7ed1be3d124921c04dbdbdcc7e5baf63c90
Anaswara T Rajan <anaswaratrajan@gmail.com>
2024-08-10 15:15:42 +0530

    fix projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,13 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
+  site: "https://anastr0.netlify.app/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
+        "https://anastr0.netlify.app/sitemap-index.xml",
+        "https://anastr0.netlify.app/sitemap-0.xml",
       ],
     }),
     solidJs(),
